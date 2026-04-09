6ae15b5510776f29c39fbcb4a12d28cbd7ef6bec
Timothy Mmbasu Juma 0004 <timothy_mmbasu.juma0004@stud.hkr.se>
2026-04-09 17:40:44 +0200

    logging setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ init_db()
 # ================================
 # STREAMLIT UI
 # ================================
-st.title("Student Exam Reminder v4")
+st.title("Student Exam Reminder v3")
 
 course = st.text_input("Course Code")
 email = st.text_input("Email")
